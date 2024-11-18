95c1e5d6073cb1ab22cdd9d041adcc73790b3cf1
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 10:55:02 -0300

    Add files via upload

---
+++ Ex2-AreaQuadrado-WinForms/Ex2-AreaQuadrado-WinForms/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Ex2_AreaQuadrado_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a aresta do quadrado:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(93, 180);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(55, 13);
+            this.Resultado.TabIndex = 1;
+            this.Resultado.Text = "Resultado";
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(12, 149);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(430, 20);
+            this.n1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
