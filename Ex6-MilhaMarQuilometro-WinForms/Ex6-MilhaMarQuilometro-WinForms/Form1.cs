eda063a90bba57d04b28579353bfafcff7b5895d
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 12:17:03 -0300

    Add files via upload

---
+++ Ex6-MilhaMarQuilometro-WinForms/Ex6-MilhaMarQuilometro-WinForms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex6_MilhaMarQuilometro_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            Milha m = new Milha();
+            m.setN1(int.Parse(n1.Text));
+            m.CalcularDistancia();
+            Resultado.Text = m.getDistancia().ToString();
+        }
+    }
+}
---
+++ Ex6-MilhaMarQuilometro-WinForms/Ex6-MilhaMarQuilometro-WinForms/Milha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_MilhaMarQuilometro_WinForms
+{
+    internal class Milha
+    {
+        private float n1;
+        private float resultado;
+
+        public void setN1(float n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void CalcularDistancia()
+        {
+            this.resultado = (n1 * 1852) / 1000;
+        }
+
+        public float getDistancia()
+        {
+            return this.resultado;
+        }
+
+    }
+}
