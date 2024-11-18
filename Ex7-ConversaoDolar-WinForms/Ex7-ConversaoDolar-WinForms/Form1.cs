d81fcb88507e9a60d830b68b0299122fd83e8fdb
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 14:41:22 -0300

    Add files via upload

---
+++ Ex7-ConversaoDolar-WinForms/Ex7-ConversaoDolar-WinForms/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex7_ConversaoDolar_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            Cotacao c = new Cotacao();
+            c.setN1(float.Parse(n1.Text));
+            c.setN2(float.Parse(n2.Text));
+            c.CalcularCotacao();
+            Resultado.Text = c.getCotacao().ToString();
+        }
+             
+    }
+}
+    
---
+++ Ex7-ConversaoDolar-WinForms/Ex7-ConversaoDolar-WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Ex7-ConversaoDolar-WinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ex7-ConversaoDolar-WinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("31cf5e04-c71f-4204-a504-ee1eac48a9ba")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
