c8254291db24e28000bc74c90f9caff469edfef4
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 11:16:59 -0300

    Add files via upload

---
+++ Ex3-AreaQuadradoDiagonal-WinForms/Ex3-AreaQuadradoDiagonal-WinForms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex3_AreaQuadradoDiagonal_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            Quadrado q = new Quadrado();
+            q.setN1(int.Parse(n1.Text));
+            q.CalcularArea();
+            Resultado.Text = q.getArea().ToString();
+        }
+    }
+}
---
+++ Ex3-AreaQuadradoDiagonal-WinForms/Ex3-AreaQuadradoDiagonal-WinForms/Quadrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_AreaQuadradoDiagonal_WinForms
+{
+    internal class Quadrado
+    {
+        private int n1;
+        private int resultado;
+
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void CalcularArea()
+        {
+            this.resultado = (n1 * n1)/2;
+        }
+
+        public int getArea()
+        {
+            return this.resultado;
+        }
+    }
+}
