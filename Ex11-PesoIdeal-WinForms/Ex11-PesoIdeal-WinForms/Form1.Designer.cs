daebd90fc824be850c0dffb45dd1ca3575a4d556
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-19 16:25:10 -0300

    Add files via upload

---
+++ Ex11-PesoIdeal-WinForms/Ex11-PesoIdeal-WinForms/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Ex11_PesoIdeal_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PESO em kg(quilogramas):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ALTURA em m(metros):";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(33, 259);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(73, 13);
+            this.result.TabIndex = 2;
+            this.result.Text = "RESULTADO";
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(31, 113);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(287, 20);
+            this.n1.TabIndex = 3;
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(31, 170);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(287, 20);
+            this.n2.TabIndex = 4;
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(31, 221);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(75, 23);
+            this.Calcular.TabIndex = 5;
+            this.Calcular.Text = "IMC";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.Button Calcular;
+    }
+}
+
---
+++ Ex11-PesoIdeal-WinForms/Ex11-PesoIdeal-WinForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex11_PesoIdeal_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            Peso p = new Peso();
+            p.setN1(double.Parse(n1.Text));
+            p.setN2(double.Parse(n2.Text));
+            p.CalcularPesoIdeal();
+            result.Text = p.getPeso().ToString();
+        }
+    }
+}
